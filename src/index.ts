57fbd5773b9b00afa3e3326f487d915a1152defb
Pavel Vorobyov <aquavitale@yandex.ru>
2020-01-18 22:49:35 +0300

    sharded submodel

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import AbstractModel, { Field, SaveRequired } from './abstract_model';
 import StorableModel from './storable_model';
 import ShardedModel from './sharded_model';
 import StorableSubmodel from './storable_submodel';
+import ShardedSubmodel from './sharded_submodel';
 import { FieldRequired, InvalidFieldType, ModelSaveRequired } from './errors';
 import db, { DBShard } from './db';
 
@@ -10,6 +11,7 @@ export {
   StorableModel,
   ShardedModel,
   StorableSubmodel,
+  ShardedSubmodel,
   FieldRequired,
   InvalidFieldType,
   ModelSaveRequired,
---
+++ src/sharded_submodel.ts
@@ -0,0 +1,101 @@
+import ShardedModel from './sharded_model';
+import AbstractModel, { Field } from './abstract_model';
+import {
+  WrongSubmodel,
+  SubmodelError,
+  MissingSubmodel,
+  UnknownSubmodel,
+} from './errors';
+
+type ShardedSubmodelConstructor<T extends ShardedSubmodel> = new (
+  ...args: any[]
+) => T;
+
+export default class ShardedSubmodel extends ShardedModel {
+  @Field() submodel: string;
+
+  static __submodel__: string | null = null;
+  static __submodel_loaders: {
+    [key: string]: ShardedSubmodelConstructor<ShardedSubmodel>;
+  } = {};
+
+  get __submodel__() {
+    return (this.constructor as typeof ShardedSubmodel).__submodel__;
+  }
+
+  constructor(shardId: string, data: { [key: string]: any } = {}) {
+    super(shardId, data);
+    if (this.isNew) {
+      if (!this.__submodel__) {
+        throw new SubmodelError(
+          `Attempted to create an object of abstract model ${this.constructor.name}`
+        );
+      }
+      if ('submodel' in data) {
+        throw new SubmodelError(
+          'Attempt to override submodel for a new object'
+        );
+      }
+      this.submodel = this.__submodel__;
+    } else {
+      if (!this.submodel) {
+        throw new MissingSubmodel(
+          `${this.constructor.name} has no submodel in DB. Bug?`
+        );
+      }
+      this._checkSubmodel();
+    }
+  }
+
+  _checkSubmodel() {
+    if (this.submodel !== this.__submodel__) {
+      throw new WrongSubmodel(
+        `Attempted to load ${this.submodel} as ${this.constructor.name}. Correct submodel would be ${this.__submodel__}. Bug?`
+      );
+    }
+  }
+
+  _validate(): void {
+    super._validate();
+    this._checkSubmodel();
+  }
+
+  protected static _preprocessQuery(query: {
+    [key: string]: any;
+  }): { [key: string]: any } {
+    if (!this.__submodel__) return query;
+    return {
+      ...query,
+      submodel: this.__submodel__,
+    };
+  }
+
+  static registerSubmodel<T extends ShardedSubmodel>(
+    name: string,
+    ctor: ShardedSubmodelConstructor<T>
+  ) {
+    if (this.__submodel__) {
+      throw new SubmodelError(
+        'Attempted to register a submodel with another submodel'
+      );
+    }
+    if (name in this.__submodel_loaders) {
+      throw new SubmodelError(`Submodel ${name} is already registered`);
+    }
+    this.__submodel_loaders[name] = ctor;
+  }
+
+  static fromData<T extends AbstractModel>(data: { [key: string]: any }): T {
+    if (!('submodel' in data)) {
+      throw new MissingSubmodel(`${this.name} has no submodel in the DB. Bug?`);
+    }
+    const submodelName = data['submodel'];
+    if (!(submodelName in this.__submodel_loaders)) {
+      throw new UnknownSubmodel(
+        `Submodel ${submodelName} is not registered with ${this.name}`
+      );
+    }
+    const ctor = this.__submodel_loaders[submodelName];
+    return (new ctor(data) as unknown) as T;
+  }
+}
